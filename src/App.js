da98cc9cc60a48488a04652cdce4806147d202db
AlexandreC93 <alexandrecadinot93@gmail.com>
2021-12-22 18:34:49 +0100

    start

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Home from './home/Home';
+import logo from './logo.svg';
+import './main.scss';
+
+function App() {
+  return (
+    <div className="App">
+
+      <Home />
+    </div>
+  );
+}
+
+export default App;

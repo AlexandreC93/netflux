cec02e7febfab121ce07f762763a715de0cdd92a
AlexandreC93 <alexandrecadinot93@gmail.com>
2021-12-24 02:03:02 +0100

    first part done

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -13,6 +13,21 @@ export default function Login() {
           />
         </div>
       </div>
+      <div className="container">
+        <form action="">
+          <h1>Sign In</h1>
+          <input type="email" placeholder="Email or phone number" />
+          <input type="password" placeholder="Password" />
+          <button className="loginButton">Sign In</button>
+          <span>
+            New to Netflix? <b>Sign up now.</b>
+          </span>
+          <small>
+            This page is protected by Google reCAPTACHA to ensure you're not a
+            bot. <b>Learn more</b>
+          </small>
+        </form>
+      </div>
     </div>
   );
 }
